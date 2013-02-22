20b2041dfa9cd8697c16e1256bed751f8495c061
Dima Pasechnik <dima@ntu.edu.sg>
2013-02-22 13:38:35 +0800

    instructions added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Integrating CSDP with Sage (and pycsdp)
+###Integrating CSDP with Sage (and pycsdp)
+
+To create a working installation, you need recent autotools.
+Run 
+    autoreconf --install 
+    automake --add-missing --copy
+
+and then 
+
+    ./configure 
+    make
+    make install # (eventually)
--- spkg/install.sh
+++ spkg/install.sh
@@ -1,15 +1,7 @@
-#! /bin/bash 
 #
 # this will eventually become the spkg-install
 #
-# 1) Compile Csdp static lib in lib/ subdirectory of Csdp distibution 
-# and install it (i.e. libcsdp.a) into $SAGE_LOCAL/lib/
-export SAGE_LOCAL=/usr/local/src/sage/current/local
-PWD=`pwd`
-cd ../lib 
+# run this under sage -sh, in the top directory of csdp
+./configure --prefix=$SAGE_LOCAL
+make
 make install
-echo $SAGE_LOCAL
-echo $PWD
-cd $PWD
-#mkdir $SAGE_LOCAL/include/sdp/
-cp ../include/*.h $SAGE_LOCAL/include/sdp/
