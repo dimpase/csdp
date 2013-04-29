c52317b0b7fd485d50931578f67e3eeafce44613
Dima Pasechnik <dima@ntu.edu.sg>
2013-04-29 16:29:02 +0800

    added support to make Sage spkg, and the corresponding files

---
+++ spkg/make-spkg.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# needs sage in your PATH
+
+#PATCHLEVEL=".p0"
+PATCHLEVEL=
+
+set -e
+if [ ! -f make-spkg.sh ] ; then
+    echo "You must run this script in the spkg directory of the csdp repository."
+    exit 1
+fi
+
+rm -rf csdp-*
+
+CWD=`pwd`
+cd ..
+
+VERSION=`autoconf --trace='AC_INIT:$2'`
+CSDP_DIR="csdp-$VERSION$PATCHLEVEL"
+SPKG_ROOT="$CWD/$CSDP_DIR"
+
+rm -rf "$SPKG_ROOT"
+mkdir -p "$SPKG_ROOT"
+
+rm -f csdp-$VERSION.tar.gz
+make dist
+
+cd "$SPKG_ROOT"
+tar xf ../../csdp-$VERSION.tar.gz
+mv csdp-$VERSION src
+
+cd "$CWD"
+cp spkg-install "$SPKG_ROOT"
+cp SPKG.txt "$SPKG_ROOT"
+cp ../LICENSE "$SPKG_ROOT"
+
+cd "$SPKG_ROOT"
+echo 'src' > .hgignore
+hg init .
+hg add
+hg commit -m 'This is not the true repository, read SPKG.txt'
+
+cd "$CWD"
+sage -pkg "$CSDP_DIR"
+
+# uncomment the follwing if you want to test
+#export SAGE_DEBUG=yes
+#sage -f -s "./$CSDP_DIR.spkg"
+
+
+
